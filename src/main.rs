46ccb2dd3c4532a1c197b6a63d21c8fc7819d658
Drew Stone <drewstone329@gmail.com>
2016-09-14 00:45:03 -0400

    Ownership hell

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ extern crate rustc_serialize;
 use web3_api::{ Web3Client };
 
 fn main() {
-	let web3 = Web3Client::new();
+	let mut web3 = Web3Client::new();
 	println!("{:?}", web3.get_accounts());
+	println!("{:?}", web3.get_work());
 }
--- src/web3_api.rs
+++ src/web3_api.rs
@@ -14,27 +14,22 @@ pub struct RpcResponse {
 
 pub struct Web3Client {
 	pub client: Client,
-	headers: Headers,
 }
 
 impl Web3Client {
 	pub fn new() -> Web3Client {
-		let mut headers = Headers::new();
-		headers.set(ContentType(Mime(TopLevel::Application, SubLevel::Json, vec![(Attr::Charset, Value::Utf8)])));
-
 		Web3Client {
 			client: Client::new(),
-			headers: headers,
 		}
 	}
 
-	pub fn get_accounts(self) -> RpcResponse {
+	pub fn get_accounts(&mut self) -> RpcResponse {
 		let accounts_command = r#"{"jsonrpc":"2.0","method":"eth_accounts","params":[],"id":1}"#;
 
 		let mut result: RpcResponse = match self.client
 		.post("http://localhost:8545")
 		.body(accounts_command)
-		.headers(self.headers)
+		.headers(get_headers())
 		.send() {
 			Ok(mut res) => {
 				let mut buf: String = String::new();
@@ -47,4 +42,30 @@ impl Web3Client {
 
 		result
 	}
+
+	pub fn get_work(&mut self) -> RpcResponse {
+		let accounts_command = r#"{"jsonrpc":"2.0","method":"eth_getWork","params":[],"id":73}"#;
+
+		let mut result: RpcResponse = match self.client
+		.post("http://localhost:8545")
+		.body(accounts_command)
+		.headers(get_headers())
+		.send() {
+			Ok(mut res) => {
+				let mut buf: String = String::new();
+				res.read_to_string(&mut buf).unwrap();
+				let decoded: RpcResponse = json::decode(&buf).unwrap();
+				decoded
+			},
+			Err(_) => panic!("Error getting work package".to_string())
+		};
+
+		result	
+	}
+}
+
+fn get_headers() -> Headers {
+	let mut headers = Headers::new();
+	headers.set(ContentType(Mime(TopLevel::Application, SubLevel::Json, vec![(Attr::Charset, Value::Utf8)])));
+	headers
 }
