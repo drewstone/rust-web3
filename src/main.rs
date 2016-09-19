9a450ada0b8934ce3d8ee025d195585df187ebf0
Drew Stone <drewstone329@gmail.com>
2016-09-19 00:38:06 -0400

    Improving reusability

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ extern crate rustc_serialize;
 use web3_api::{ Web3Client };
 
 fn main() {
-	let mut web3 = Web3Client::new();
+	let mut web3 = Web3Client::new("http://localhost:8545".to_string());
 	println!("{:?}", web3.get_accounts());
 	println!("{:?}", web3.get_work());
 }
--- src/web3_api.rs
+++ src/web3_api.rs
@@ -2,6 +2,7 @@ use hyper::{Client};
 use std::io::Read;
 use hyper::header::{Headers, ContentType};
 use hyper::mime::{Mime, TopLevel, SubLevel, Attr, Value};
+use hyper::client::response::Response;
 
 use rustc_serialize::json;
 
@@ -14,54 +15,51 @@ pub struct RpcResponse {
 
 pub struct Web3Client {
 	pub client: Client,
+	pub url: String,
 }
 
 impl Web3Client {
-	pub fn new() -> Web3Client {
+	pub fn new(url: String) -> Web3Client {
 		Web3Client {
 			client: Client::new(),
+			url: url
 		}
 	}
 
-	pub fn get_accounts(&mut self) -> RpcResponse {
-		let accounts_command = r#"{"jsonrpc":"2.0","method":"eth_accounts","params":[],"id":1}"#;
-
-		let mut result: RpcResponse = match self.client
-		.post("http://localhost:8545")
-		.body(accounts_command)
+	fn request(&mut self, command: String) -> RpcResponse {
+		let result: RpcResponse = match self.client
+		.post(&self.url)
+		.body(&command)
 		.headers(get_headers())
 		.send() {
-			Ok(mut res) => {
-				let mut buf: String = String::new();
-				res.read_to_string(&mut buf).unwrap();
-				let decoded: RpcResponse = json::decode(&buf).unwrap();
-				decoded
-			},
+			Ok(res) => decode_response(res),
 			Err(_) => panic!("error".to_string()),
 		};
 
 		result
 	}
 
+	pub fn client_version(&mut self) -> RpcResponse {
+		let result: RpcResponse = self.request(r#"{"jsonrpc":"2.0","method":"web3_clientVersion","params":[],"id":67}"#.to_string());
+		result
+	}
+
+	pub fn get_accounts(&mut self) -> RpcResponse {
+		let result: RpcResponse = self.request(r#"{"jsonrpc":"2.0","method":"eth_accounts","params":[],"id":1}"#.to_string());
+		result
+	}
+
 	pub fn get_work(&mut self) -> RpcResponse {
-		let accounts_command = r#"{"jsonrpc":"2.0","method":"eth_getWork","params":[],"id":73}"#;
-
-		let mut result: RpcResponse = match self.client
-		.post("http://localhost:8545")
-		.body(accounts_command)
-		.headers(get_headers())
-		.send() {
-			Ok(mut res) => {
-				let mut buf: String = String::new();
-				res.read_to_string(&mut buf).unwrap();
-				let decoded: RpcResponse = json::decode(&buf).unwrap();
-				decoded
-			},
-			Err(_) => panic!("Error getting work package".to_string())
-		};
-
+		let result: RpcResponse = self.request(r#"{"jsonrpc":"2.0","method":"eth_getWork","params":[],"id":73}"#.to_string());
 		result	
 	}
+}
+
+fn decode_response(mut response: Response) -> RpcResponse {
+	let mut buf: String = String::new();
+	response.read_to_string(&mut buf).unwrap();
+	let decoded: RpcResponse = json::decode(&buf).unwrap();
+	decoded
 }
 
 fn get_headers() -> Headers {
